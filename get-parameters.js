9a5aa7fddd94b6e2a255bba18619ddac5cc2bc63
Matheus Alves <theuves@gmail.com>
2021-02-10 19:24:56 -0400

    style: some updates

--- get-parameters.js
+++ get-parameters.js
@@ -1,45 +1,35 @@
-const SSM = require('aws-sdk/clients/ssm');
+const AWSSSM = require('aws-sdk/clients/ssm')
+const ssmClient = new AWSSSM()
 
-const ssm = new SSM();
+module.exports = function getParameters(path, isRecursive = false) {
+  return new Promise((resolve, reject) => {
+    const allParameters = []
+    const getParametersByPath = (path, nextToken = undefined) => {
+      const options = {
+        Path: path,
+        WithDecryption: true,
+        Recursive: isRecursive,
+        NextToken: nextToken,
+      }
 
-function getParameters(path, isRecursive = false) {
-    return new Promise((resolve, reject) => {
-        const allParameters = [];
+      ssmClient.getParametersByPath(options, (err, data) => {
+        if (err) {
+          reject(new Error('Unable to get the parameters.'))
+        } else {
+          const parameters = data
+            .Parameters
+            .map(({ Name, Value }) => ({
+              name: Name,
+              value: Value,
+            }))
 
-        const getParametersByPath = (path, nextToken = undefined) => {
-            const options = {
-                Path: path,
-                WithDecryption: true,
-                Recursive: isRecursive,
-                NextToken: nextToken,
-            };
-
-            ssm.getParametersByPath(options, (err, data) => {
-                if (err) {
-                    reject(new Error('Unable to get the parameters.'));
-                    return;
-                }
-
-                const parameters = data
-                    .Parameters
-                    .map(({ Name, Value }) => ({
-                        name: Name,
-                        value: Value,
-                    }));
-
-                allParameters.push(parameters);
-
-                if (data.NextToken) {
-                    getParametersByPath(path, data.NextToken);
-                } else {
-                    resolve(allParameters);
-                    return;
-                }
-            });
+          allParameters.push(parameters)
+          if (data.NextToken) getParametersByPath(path, data.NextToken)
+          else resolve(allParameters)
         }
+      })
+    }
 
-        getParametersByPath(path);
-    });
+    getParametersByPath(path)
+  })
 }
-
-module.exports = getParameters;
--- index.js
+++ index.js
@@ -1,60 +1,50 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const minimist = require('minimist');
-const { version } = require('./package.json');
-const getParameters = require('./get-parameters');
-const opts = minimist(process.argv.slice(2));
-const path = opts._[0];
-
-if (!path) {
-  logError(`Usage: psenv <PATH> [OPTION]...
-Try 'psenv --help' to more information.`);
-}
+const fs = require('fs')
+const minimist = require('minimist')
+const { version } = require('./package.json')
+const getParameters = require('./get-parameters')
+const opts = minimist(process.argv.slice(2))
+const path = opts._[0]
 
 if (opts.v || opts.version) {
-  logMessage("v%s", version);
+  logInfo(`v${version}`)
 }
 
 if (!path || options.help) {
-  logMessage(`Usage: psenv <PATH> [OPTION]...
+  logInfo(`Usage: psenv <PATH> [OPTION]...
 
 Options:
-  --output=FILENAME  Write to a file (e.g. --output=.env)
-  --to-upper-case    Convert the name to upper case (e.g. name to NAME)
-  --recursive        Retrieve all parameters within a hierarchy
-  --is-dotenv        Output with the format NAME=value
-  --is-cmd           Output for Windows Command Prompt (cmd.exe)
-  -h, --help         Print this message
-  -v, --version      Print the current version of psenv`);
+  --output=FILENAME  write to a file (e.g. --output=.env)
+  --to-upper-case    convert the name to upper case (e.g. name to NAME)
+  --recursive        retrieve all parameters within a hierarchy
+  --is-dotenv        output with the format NAME=value
+  --is-cmd           output for Windows Command Prompt (cmd.exe)
+  -h, --help         print this message
+  -v, --version      print the current version of psenv`)
+}
+
+if (!path) {
+  logInfo(`Usage: psenv <PATH> [OPTION]...
+Try 'psenv --help' to more information.`)
 }
 
 getParameters(path, opts.recursive)
   .then(processParameters)
-  .catch(() => logError('Unable to get parameters.'));
-
-function logMessage(message) {
-  console.log(message);
-  process.exit(0);
-}
-
-function logError(message) {
-  console.error(`[error] ${message}`);
-  process.exit(1);
-}
+  .catch(() => logError('Unable to get parameters.'))
 
 function processParameters(parameters) {
   const variables = parameters
-    .flat() // The initial 'parameters' value will be Array of Arrays
+    .flat() // The initial 'parameters' value will be a array of arrays
     .map(({ name, value }) => toVariable(name, value))
-    .join('\n');
+    .join('\n')
 
   if (opts.output) {
     fs.writeFile(opts.output, raw, (error) => {
-      if (error) logError(`Unable to create the file.`);
-      logMessage('File is created successfully.');
+      if (error) logError(`Unable to create the file.`)
+      logInfo('File is created successfully.')
     })
   } else {
-    logMessage(variables);
+    logInfo(variables)
   }
 }
 
@@ -66,17 +56,27 @@ function toVariable(name, value) {
     return `${name}=${value}`
   if (opts.isCmd)
     return `set "${name}=${value}"`
-
   return `export ${name}=${value}`
 }
 
 /*
  * Get the "environment variable" name from path
  * @example
- * getName('/foo/bar/BAZ') === 'BAZ'
+ * getName('/foo/bar/BAZ')
+ * // 'BAZ'
  */
 function getName(path) {
   return path
     .split('/')
-    .reverse()[0];
+    .reverse()[0]
+}
+
+function logInfo(message) {
+  console.log(message)
+  process.exit(0)
+}
+
+function logError(message) {
+  console.error('[error] %s', message)
+  process.exit(1)
 }
